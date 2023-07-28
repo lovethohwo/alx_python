a549e70dae65860b1c7bdf3f11e23c686358fb19
judyloveth <judyohwo29@gmail.com>
2023-07-28 23:12:29 +0100

    1-edges file

---
+++ 1-edges.py
@@ -0,0 +1,8 @@
+word = "Holberton"
+l=len(word)
+print("First 3 letters: {}".format(word[0 : 3]))
+print("Last 2 letters: {}".format(word[-2:]))
+print("middle word: {}".format(word[1:-1]))
+
+
+      
