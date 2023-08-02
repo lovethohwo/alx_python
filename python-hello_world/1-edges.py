219b854000725da15623c6ee0ebd4722dc09491c
judyloveth <judyohwo29@gmail.com>
2023-08-02 15:03:35 +0100

    updated letter words

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
@@ -5,6 +6,3 @@ middle_word = word[1:-1]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("middle word: {}".format(middle_word))
-
-
-      
