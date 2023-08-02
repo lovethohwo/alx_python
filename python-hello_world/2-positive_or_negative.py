07c9ae0024c8cdc4f11ae35daad1c53be0ea9be6
judyloveth <judyohwo29@gmail.com>
2023-08-02 12:45:21 +0100

    updated printing of positive and negative number

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,18 +1,13 @@
-#word = "Holberton"
-'''l=len(word)
-print("First 3 letters: {}".format(word[0 : 3]))
-print("Last 2 letters: {}".format(word[-2:]))
-print("middle word: {}".format(word[1:-1]))
-'''
+
 import random
 
 number = random.randint(-10, 10)
 
 if number < 0:
-    print("{} : is negative.".format(number))
+    print("{} is posituve".format(number))
 elif number == 0:
-    print("{} : is zero.".format(number))
+    print("{} is zero".format(number))
 else:
-    print("{} : is positive.".format(number))
+    print("{} is negative".format(number))
 
       
