a0f911266f97db955af16560ec82dac4e8954050
judyloveth <judyohwo29@gmail.com>
2023-08-02 18:18:33 +0100

    updated

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,8 @@
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        print("{:02d}".format(tens_digit * 10 + ones_digit), end=', ')
-print("\n")
+
+
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:02d}".format(i * 10 + j))
+    else:
+        print(" {:02d}, ".format(i * 10 + j), end="")
