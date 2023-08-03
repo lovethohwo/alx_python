a87a23701e12314046754f216bd73a79f896cd96
judyloveth <judyohwo29@gmail.com>
2023-08-04 00:20:43 +0100

    new update of argument

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,19 +1,19 @@
 
 import sys
 
-def print_argument(argv):
+def print_arguments(argv):
     num_args = len(argv)
 
     if num_args == 0:
-        print("0 arguments.")
+        print("0 argument.")
         print(":")
     else:
-        print(f"{num_args} arguments{'s' if num_args > 1 else ''}:")
+        print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
         for i, arg in enumerate(argv, 1):
             print(f"{i}: {arg}")
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    print_argument(args)
+    print_arguments(args)
 
         
