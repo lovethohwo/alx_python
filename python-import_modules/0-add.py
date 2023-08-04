25c8faa7c93cf032e598e013c04cd023fb7dddc1
judyloveth <judyohwo29@gmail.com>
2023-08-04 01:30:08 +0100

    new update on import

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,13 @@
 # Importing add function from another file
 from add_0 import add
 
-def outcome():
+def result():
     a = 1
     b = 2
-    total = add(a,b)
-    print("{} + {} = {}".format(a,b, total))
+    total = add(a, b)
+    print("{} + {} = {}".format(a, b, total))
 
 if __name__ == "__main__":
-    outcome()
+    result()
 
 
