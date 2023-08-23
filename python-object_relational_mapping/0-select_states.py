bbad7fc6dc2e85bddba95ab354c90a718858a196
judyloveth <judyohwo29@gmail.com>
2023-08-23 07:02:15 +0100

    updated file zero

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,5 @@
-# 0-select_states.py
+# 0-select_states.pyd
+import MySQLdb
+database = MySQLdb.connect(host="localhost", user="root", passwd="Judyloveth@2023", db="hbtn_0e_0_usa")
+cursor = database.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL)")
