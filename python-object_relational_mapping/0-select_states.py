bb6da336c3b74f3016fed42d88144e44497cd70a
judyloveth <judyohwo29@gmail.com>
2023-08-23 10:22:21 +0100

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,10 @@
 # 0-select_states.py
 import MySQLdb
-database = MySQLdb.connect(
-host="localhost",
-user="root", 
-passwd="Judyloveth@2023", 
-db="hbtn_0e_0_usa")
+database = MySQLdb.connect(host="localhost", user="root", passwd="Judyloveth@2023", db="hbtn_0e_0_usa")
 cursor = database.cursor()
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL)")
+cursor.execute("CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL)")
 cursor.execute('INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada")')
-rows = cursor.execute(
-    "SELECT * FROM states")
+rows = cursor.execute("SELECT * FROM states")
 results = cursor.fetchall()
 for result in results:
     print(result)
