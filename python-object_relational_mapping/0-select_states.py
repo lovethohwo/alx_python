ea66b42303936b2d917b92e38f03f4619b12d1f7
judyloveth <judyohwo29@gmail.com>
2023-08-23 12:27:59 +0100

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,15 @@
 import MySQLdb
+#Database parameters
+host="localhost"
+user="root"
+passwd="Judyloveth@2023"
+db="hbtn_0e_0_usa"
+# Connect to the MySQL server
+db = MySQLdb.connect(host, user, passwd, db)
+cursor = db.cursor()
 
 # Execute the query to retrieve states
-query = "SELECT * FROM states ORDER BY id ASC"
+query = "SELECT * FROM states ORDER BY states.id ASC"
 cursor.execute(query)
 
 # Fetch all the results
