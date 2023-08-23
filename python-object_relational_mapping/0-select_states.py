b64ca12f98d2f52aa263799aab07a26ba8c065a8
judyloveth <judyohwo29@gmail.com>
2023-08-23 13:15:30 +0100

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,17 +4,18 @@ host="localhost"
 user="root"
 passwd="Judyloveth@2023"
 db="mysql"
+port=3306
 
 # Connect to the MySQL server
-db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
+db = MySQLdb.connect(host=host, user=user, passwd=passwd, port=port, db=db)
 cursor = db.cursor()
 
 create = "CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL)"
 cursor.execute(create)
 
 # Insert
-qry = 'INSERT INTO states (name) VALUES ("California"), ("Arizona")'
-cursor.execute(qry)
+#qry = 'INSERT INTO states (name) VALUES ("California"), ("Arizona")'
+#cursor.execute(qry)
 
 # Execute the query to retrieve states
 query = "SELECT * FROM states ORDER BY id ASC"
