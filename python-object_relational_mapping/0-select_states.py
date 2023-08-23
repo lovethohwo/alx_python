7ec7369e97cc623beceee94899ec61fa81ff0519
judyloveth <judyohwo29@gmail.com>
2023-08-23 14:33:18 +0100

    updated

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,49 @@
 import MySQLdb
+
+# Connect to the MySQL database
 #Database parameters
+'''
 host="localhost"
 user="root"
 passwd="Judyloveth@2023"
 db="mysql"
 port=3306
+'''
+conn = MySQLdb.connect(
+    host="localhost",
+    user="root",
+    passwd="Judyloveth@2023",
+    db="mysql"
+)
+cursor = conn.cursor()
 
-# Connect to the MySQL server
-db = MySQLdb.connect(host=host, user=user, passwd=passwd, port=port, db=db)
-cursor = db.cursor()
+# Create the table if it doesn't exist
+create_table_query = '''
+    CREATE TABLE IF NOT EXISTS my_table (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) UNIQUE
+    )
+'''
+cursor.execute(create_table_query)
+conn.commit()
 
-create = "CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL)"
-cursor.execute(create)
+def insert_entry():
+    try:
+        # Attempt to insert the entry into the table
+        insert_query = '''
+            INSERT INTO my_table (name)
+            VALUES ("Texas"),("Arizona")
+        '''
+        cursor.execute(insert_query)
+        conn.commit()
+        print("Entry inserted successfully.")
+    except MySQLdb.IntegrityError as e:
+        print("Error:", e)
+        print("Duplicate entry detected. Entry not inserted.")
 
-# Insert
-qry = 'INSERT INTO states (name) VALUES ("California"), ("Arizona")'
-cursor.execute(qry)
+# Example usage
+insert_entry()
+  # This will result in a duplicate entry error
 
-# Execute the query to retrieve states
-query = "SELECT * FROM states ORDER BY id ASC"
-cursor.execute(query)
-
-# Fetch all the results
-results = cursor.fetchall()
-
-# Print the results
-for row in results:
-    print(row)
-
-# Close the cursor and the database connection
-cursor.close()
-db.close()
+# Close the connection
+conn.close()
