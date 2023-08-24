5e61e2aa73beba97c1b4677d5db27a296eb2a75b
judyloveth <judyohwo29@gmail.com>
2023-08-24 09:16:41 +0100

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -13,9 +13,29 @@ cursor = db.cursor()
 create = "CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL)"
 cursor.execute(create)
 
-# Insert
-qry = 'INSERT INTO states (name) VALUES ("California"), ("Arizona")'
-cursor.execute(qry)
+def insert_entry():
+    try:
+        # Insert
+        qry = 'INSERT INTO states (name) VALUES ("California"), ("Arizona")'
+        cursor.execute(qry)
+        # Attempt to insert the entry into the table
+        '''
+        insert_query = 
+            INSERT INTO my_table (name)
+            VALUES ("Texas"),("Arizona")
+        
+        cursor.execute(insert_query)
+        conn.commit()
+        print("Entry inserted successfully.")
+        '''
+    except MySQLdb.IntegrityError as e:
+        print("Error:", e)
+        print("Duplicate entry detected. Entry not inserted.")
+        
+# Example usage
+insert_entry()
+  # This will result in a duplicate entry error
+
 
 # Execute the query to retrieve states
 query = "SELECT * FROM states ORDER BY id ASC"
