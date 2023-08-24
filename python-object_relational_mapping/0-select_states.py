dd21e0f0155cf0caaec8b84a2225790d8a15cb2a
judyloveth <judyohwo29@gmail.com>
2023-08-24 19:19:42 +0100

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,26 @@
 import MySQLdb
-#Database parameters
-host="localhost"
-user="root"
-passwd="Judyloveth@2023"
-db="mysql"
-port=3306
 
 # Connect to the MySQL server
-db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
+db = MySQLdb.connect(host="localhost", user="root", passwd="Judyloveth@2023", db="hbtn_0c_0")
 cursor = db.cursor()
 
-create = "CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL)"
+create = '''  
+    CREATE TABLE IF NOT EXISTS states (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+        name VARCHAR(256) UNIQUE NOT NULL
+        )
+ '''
 cursor.execute(create)
+db.commit()
 
 def insert_entry():
     try:
-        # Insert
+        # Attempt to insert the entry into the table
         qry = 'INSERT INTO states (name) VALUES ("California"), ("Arizona")'
         cursor.execute(qry)
-        # Attempt to insert the entry into the table
-        '''
-        insert_query = 
-            INSERT INTO my_table (name)
-            VALUES ("Texas"),("Arizona")
-        
-        cursor.execute(insert_query)
-        conn.commit()
+        db.commit()
         print("Entry inserted successfully.")
-        '''
+        
     except MySQLdb.IntegrityError as e:
         print("Error:", e)
         print("Duplicate entry detected. Entry not inserted.")
--- python-object_relational_mapping/0-select_states_real.py
+++ python-object_relational_mapping/0-select_states_real.py
@@ -1,33 +1,44 @@
 import MySQLdb
-#Database parameters
+import sys
+
 host="localhost"
 user="root"
 passwd="Judyloveth@2023"
 db="mysql"
 port=3306
 
-# Connect to the MySQL server
-db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
-cursor = db.cursor()
+def list_states(user, passwd, db):
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=passwd, db=db)
+        
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
+        
+        # Execute the query to retrieve states
+        query = "SELECT * FROM states ORDER BY id ASC"
+        cursor.execute(query)
+        
+        # Fetch all the results
+        results = cursor.fetchall()
+        
+        # Display the results
+        for row in results:
+            print(row)
+        
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+        
+    except MySQLdb.Error as e:
+        print("Error:", e)
+        sys.exit(1)
 
-create = "CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL)"
-cursor.execute(create)
-
-# Insert
-qry = 'INSERT INTO states (name) VALUES ("California"), ("Arizona")'
-cursor.execute(qry)
-
-# Execute the query to retrieve states
-query = "SELECT * FROM states ORDER BY id ASC"
-cursor.execute(query)
-
-# Fetch all the results
-results = cursor.fetchall()
-
-# Print the results
-for row in results:
-    print(row)
-
-# Close the cursor and the database connection
-cursor.close()
-db.close()
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
+    else:
+        user = sys.argv[1]
+        passwd = sys.argv[2]
+        db = sys.argv[3]
+        list_states(user, passwd, db)
