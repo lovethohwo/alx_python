9efdb164bc33465e6af9d9cd71704ff9d38bf51b
judyloveth <judyohwo29@gmail.com>
2023-08-25 12:47:46 +0100

    reverse string

---
+++ python-functions/3-string.py
@@ -0,0 +1,9 @@
+#reverse string
+def reverse_string(string):
+    return string[::-1]
+
+
+(reverse_string("Hello"))
+(reverse_string(""))
+(reverse_string("madam"))
+(reverse_string("Hello, World!"))
