b9dfaea95f67b7ff270806cde9a19ea4a1a9f1f4
judyloveth <judyohwo29@gmail.com>
2023-08-31 00:15:37 +0100

    3-base geo update

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,44 +1,27 @@
 """
-    Check if the given object is an instance of a class
-     Also if it is inherited from the specified class.
+    A base class representing geometry.
 
-    Parameters:
-        obj: Any Python object.
-        a_class:class name to compare the type of the object against.
-
-    Returns:
-    True if the object is an instance of a subclass of the specified class;
-    Otherwise False.
-    Example:
-        >>> class Animal:
-        ...     pass
-        ...
-        >>> class Dog(Animal):
-        ...     pass
-        ...
-        >>> class Cat(Animal):
-        ...     pass
-        ...
-        >>> class Bird:
-        ...     pass
-        ...
-        >>> obj1 = Dog()
-        >>> obj2 = Cat()
-        >>> obj3 = Bird()
-        >>>
-        >>> inherits_from(obj1, Animal)
-        True
-        >>> inherits_from(obj2, Animal)
-        True
-        >>> inherits_from(obj3, Animal)
-        False
+    This class is intended to be used as a base for other geometry-related classes.
+    It currently does not have any attributes or methods defined.
     """
 
 
-def inherits_from(obj, a_class):
-    """Check if the given object is an instance of a class
-    Also if it is inherited from the specified class."""
-    return (
-        issubclass(type(obj), a_class) and
-        type(obj) != a_class
-    )
+class BaseGeometry:
+    """
+    A base class representing geometry.
+    """
+
+
+class BaseGeometryMetaClass(type):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=BaseGeometryMetaClass):
+    """
+    A base class representing geometry
+    """
+
+    def __dir__(self):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    pass
